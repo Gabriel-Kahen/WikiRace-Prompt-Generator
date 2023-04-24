3a78c692028a354d7f9b580bdce5e6b3bf26d366
Gabriel-Kahen <93809974+Gabriel-Kahen@users.noreply.github.com>
2023-04-24 14:09:30 -0400

    Update wikiRace.js

--- wikiRace.js
+++ wikiRace.js
@@ -9,7 +9,7 @@ class WikiRace {
 	start() {
 		const start = this.infoArray[Math.floor(Math.random() * this.infoArray.length)];
 		const destination = this.infoArray[Math.floor(Math.random() * this.infoArray.length)];
-		const ret = `Start: ${start}   (<a href = https://en.wikipedia.org/wiki/${start}>https://en.wikipedia.org/wiki/${start}<\a>)<br>Destination: ${destination}`;
+		const ret = `Start: ${start}   (<a href = https://en.wikipedia.org/wiki/${start}>https://en.wikipedia.org/wiki/${start}<\a>)<br>Destination: ${destination}` style="color:#36AE7C;";
 		document.getElementById('ret').innerHTML = ret;
 	}
 }
