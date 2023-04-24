c01351bc37706843f665e6c0bdec03a1b9dee983
Gabriel-Kahen <93809974+Gabriel-Kahen@users.noreply.github.com>
2023-04-23 23:33:50 -0400

    Add files via upload

---
+++ wikiRace.js
@@ -0,0 +1,20 @@
+class WikiRace {
+	constructor() {
+		this.infoArray = [];
+		fetch('infoArray.txt')
+			.then(response => response.text())
+			.then(data => {this.infoArray = data.split('\n');});
+	}
+	
+	start() {
+		const start = this.infoArray[Math.floor(Math.random() * this.infoArray.length)];
+		const destination = this.infoArray[Math.floor(Math.random() * this.infoArray.length)];
+		const ret = `Start: ${start}   (<a href = https://en.wikipedia.org/wiki/${start}>https://en.wikipedia.org/wiki/${start}<\a>)<br>Destination: ${destination}`;
+		document.getElementById('ret').innerHTML = ret;
+	}
+}
+
+const wikiRace = new WikiRace();
+document.getElementById('goobsta').addEventListener('click', () => {
+	wikiRace.start();
+});
